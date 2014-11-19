7133fd506ca40102d55c00a78b3b833aa76ef294
Pozsi <pozsi@gawker.com>
2014-11-19 23:52:21 +0100

    add html boilerplate

--- admin/templates/form.php
+++ admin/templates/form.php
@@ -1,9 +1,20 @@
-<form action="" method="post">
-    <label for="gallery_name">Név: </label>
-    <input id="gallery_name" type="text" name="name" value="<?= $name ?>" /><br />
-    <label for="gallery_data">Név: </label>
-    <textarea id="gallery_data" name="data" cols="200" rows="30">
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <title>Galeria Admin</title>
+</head>
+
+<body id="index" class="home">
+</html>
+    <form action="" method="post">
+        <label for="gallery_name">Név: </label>
+        <input id="gallery_name" type="text" name="name" value="<?= $name ?>" /><br />
+        <label for="gallery_data">Név: </label>
+        <textarea id="gallery_data" name="data" cols="200" rows="30">
 <?= $data ?>
-    </textarea><br />
-    <input type="submit" value="Mehet">
-</form>
+        </textarea><br />
+        <input type="submit" value="Mehet">
+    </form>
+</body>
