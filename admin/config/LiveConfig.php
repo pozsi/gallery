d4d8a7ccc837fee134d92b04c203cc0c700cb69c
Pozsi <pozsi@gawker.com>
2015-03-02 19:40:11 +0100

    fix paths

--- admin/config/LiveConfig.php
+++ admin/config/LiveConfig.php
@@ -6,13 +6,13 @@ class LiveConfig extends Config {
 
     function __construct($host) {
         $this->appPath = '/www/b4.hu/';
-        $this->appUrl = 'http://' . $host . 'b4.hu/';
+        $this->appUrl = 'http://' . $host . '.b4.hu/';
         $this->hostDir = $host . '/';
         $this->adminPath = $this->appPath . 'admin/';
         $this->adminUrl = $this->appUrl . 'admin/';
         $this->galleryDataPath = $this->adminPath . $this->hostDir ."galleries/";
         $this->templatePath = $this->adminPath . 'view/';
-        $this->customTemplatePath = $this->adminPath . $this->hostDir .'view/';
+        $this->customTemplatePath = $this->adminPath . 'view/' . $this->hostDir;
     }
 
 }
