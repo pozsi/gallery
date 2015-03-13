63a998c29f882ba5e7a5f3de12c1bdf76d44397b
Pozsi <pozsika@gmail.com>
2015-03-13 17:50:09 +0000

    blumi changes

--- admin/config/LiveConfig.php
+++ admin/config/LiveConfig.php
@@ -7,7 +7,7 @@ class LiveConfig extends Config {
     function __construct($host) {
         $this->host = $host;
         $this->appPath = '/www/' . $this->host . '.b4.hu/';
-        $this->appUrl = 'http://' . $this->host . '.b4.hu/';
+        $this->appUrl = 'http://' . $this->host . '.vblog.hu/';
         $this->adminPath = '/www/b4.hu/admin/';
         $this->adminUrl = 'http://b4.hu/admin/';
         $this->galleryDataPath = $this->adminPath . "galleries/" . $this->host . '/';
